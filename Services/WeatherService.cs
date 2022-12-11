0c877323653ec49fbd1caee28d62b4487d87fb62
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-12-11 23:18:21 +0300

    Modify weather service.

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Json;
+﻿using System.Text.Json.Nodes;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.WebUtilities;
 
 using WeatherApp.Options;
 using WeatherApp.Models.Dto;
@@ -16,18 +18,73 @@ namespace WeatherApp.Services
             _httpClient = client;
         }
 
-        public async Task<ForecastDto> GetForecast()
+        public async Task<T> GetAsync<T>(Uri uri)
         {
-            var response = await _httpClient.GetFromJsonAsync<ForecastDto>(_options.ForecastEndpoint);
+            try
+            {
+                var response = await _httpClient.GetAsync(uri);
 
-            return response;
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(T);
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<T>();
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var error = JsonNode.Parse(content)!["error"];
+
+                    throw new Exception($"Http status code: {response.StatusCode} code: {error!["code"]} message: {error!["message"]}");
+                }
+            }
+            catch (Exception)
+            {
+                //Log exception
+                throw;
+            }
         }
 
-        public async Task<AstronomyDto> GetAstronomy()
+        public async Task<ForecastDto> GetForecastAsync(string location, int days)
         {
-            var response = await _httpClient.GetFromJsonAsync<AstronomyDto>(_options.AstronomyEndpoint);
+            var parameters = new Dictionary<string, string>
+            {
+                // API key auth
+                { "auth_key", "auth_value" },
 
-            return response;
+                { "q", location},
+                { "days", days.ToString() }
+            };
+
+            return await GetAsync<ForecastDto>(
+                BuildUri(_options.ForecastEndpoint, parameters));
+        }
+
+        public async Task<AstronomyDto> GetAstronomyAsync(string location)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                // API key auth
+                { "auth_key", "auth_value" },
+
+                { "q", location}
+            };
+
+            return await GetAsync<AstronomyDto>(
+                BuildUri(_options.AstronomyEndpoint, parameters));
+        }
+
+        private Uri BuildUri(string endpoint, IDictionary<string, string> queryParams)
+        {
+            string uri = _httpClient.BaseAddress + endpoint;
+
+            uri = QueryHelpers.AddQueryString(uri, queryParams);
+
+            var builder = new UriBuilder(uri);
+            return builder.Uri;
         }
     }
 }
