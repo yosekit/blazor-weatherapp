df8c5b0c92a18bfc79fa4097c79f0d033075b3b1
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-12-25 16:04:41 +0300

    Revert "Api: fix routing and API key auth in WeatherContoller."
    This reverts commit 775549c907b4d79daba93946a4084797d32cc78a.

--- WeatherApp.Api/Controllers/WeatherController.cs
+++ WeatherApp.Api/Controllers/WeatherController.cs
@@ -9,9 +9,9 @@ namespace WeatherApp.Api.Controllers
 {
     [ApiController]
     [Route("api/weather")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherController> _logger;
         private readonly WeatherSettings _settings;
 
         private HttpProxyOptions _proxyOptions = HttpProxyOptionsBuilder.Instance
@@ -19,34 +19,36 @@ namespace WeatherApp.Api.Controllers
             .WithBeforeSend((context, message) =>
             {
                 var settings = context.RequestServices.GetRequiredService<WeatherSettings>();
+                context.Request.QueryString.Add(settings.Auth!.Key!, settings.Auth!.Value!);
 
-                // Add to query params API key auth 
-                message.RequestUri = new Uri(message.RequestUri + context.Request.QueryString.Add(
-                        settings.Auth!.Key!, settings.Auth!.Value!).Value);
+                /*message.RequestUri = new Uri(
+                    message.RequestUri!.ToString() +
+                    $"?{settings.Auth!.Key}={settings.Auth!.Value}");*/
 
                 return Task.CompletedTask;
             })
             .Build();
 
-        public WeatherForecastController(
-            ILogger<WeatherForecastController> logger, WeatherSettings settings)
+        public WeatherController(
+            ILogger<WeatherController> logger, WeatherSettings settings)
         {
             _logger = logger;
             _settings = settings;
         }
 
         [HttpGet]
-        [Route("forecast")]
+        [Route("/forecast")]
         public Task GetForecast()
         {
-            return this.HttpProxyAsync(_settings.Forecast, _proxyOptions);
+            _logger.LogInformation("FORECAST: " + _settings.Forecast + Request.QueryString.Value);
+            return this.HttpProxyAsync(_settings.Forecast + Request.QueryString.Value, _proxyOptions);
         }
 
         [HttpGet]
-        [Route("astronomy")]
+        [Route("/astronomy")]
         public Task GetAstronomy()
         {
-            return this.HttpProxyAsync(_settings.Astronomy, _proxyOptions);
+            return this.HttpProxyAsync(_settings.Astronomy + Request.QueryString.Value, _proxyOptions);
         }
     }
 }
